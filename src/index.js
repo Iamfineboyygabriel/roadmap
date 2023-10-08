ee73e09de330cb3caa91e3897b0beb60ab5d5e90
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-10-08 14:47:16 +0100

    a simple card tutorial for begginers using react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Skills = [
+  {
+    skill: "HTML + CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+
+  {
+    skill: "Javascript",
+    level: "intermediate",
+    color: "#EFD81D",
+  },
+
+  {
+    skill: "Web design",
+    level: "advanced",
+    color: "#2662EA",
+  },
+
+  {
+    skill: "Git and Github",
+    level: "advanced",
+    color: "#FF3B00",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60D8Fb",
+  },
+
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <Skilllist />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="rafiu.JPG" alt="Rafiu" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Son Of God</h1>
+      <p>
+        Full stack web developer and teacher at udemy. when not coding or
+        preparing a course , i like to play board games , to cook (and eat) , or
+        to just enjoy the Portuguese sun at the beach
+      </p>
+    </div>
+  );
+}
+
+function Skilllist() {
+  return (
+    <div className="skill">
+      {Skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "😒😒"}
+        {level === "intermediate" && "😊"}
+        {level === "advanced" && "😍"}
+      </span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
